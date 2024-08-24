a1a0b41da4dc1d93ad98ce0236d121d327d032a5
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-24 09:33:06 -0300

    feat: 'create entity Disease that represents diseases table'

---
+++ src/main/java/com/zex/spring_security/domain/disease/Disease.java
@@ -0,0 +1,29 @@
+package com.zex.spring_security.domain.disease;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "diseases")
+@Entity(name = "disease")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+
+public class Disease {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String cid;
+
+    private String name;
+
+    private String symptoms;
+
+    private String treatments;
+
+    private Integer risk;
+}
