3d3cc946c35c9fb171ebe7289859b71771270ce5
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-24 09:48:21 -0300

    feat: 'create save method to persist Disease on databse'

--- src/main/java/com/zex/spring_security/services/DiseaseService.java
+++ src/main/java/com/zex/spring_security/services/DiseaseService.java
@@ -1,5 +1,8 @@
 package com.zex.spring_security.services;
 
+import com.zex.spring_security.domain.disease.Disease;
+import com.zex.spring_security.domain.disease.DiseaseRequest;
+import com.zex.spring_security.domain.disease.DiseaseResponse;
 import com.zex.spring_security.repositories.DiseaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +13,11 @@ public class DiseaseService {
     private DiseaseRepository repository;
 
 
+    public DiseaseResponse save(DiseaseRequest data) {
+        Disease newDisease = new Disease(data);
 
+        return new DiseaseResponse(this.repository.save(newDisease));
+    }
 
 
 }
