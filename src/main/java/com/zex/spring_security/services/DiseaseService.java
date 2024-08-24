a27626655592454579cdc628ed0f18f2ba74043c
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-24 09:51:16 -0300

    feat: 'create a getAll method to get all Diseases on database'

--- src/main/java/com/zex/spring_security/services/DiseaseService.java
+++ src/main/java/com/zex/spring_security/services/DiseaseService.java
@@ -7,6 +7,9 @@ import com.zex.spring_security.repositories.DiseaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class DiseaseService {
     @Autowired
@@ -17,6 +20,15 @@ public class DiseaseService {
         Disease newDisease = new Disease(data);
 
         return new DiseaseResponse(this.repository.save(newDisease));
+    }
+
+
+    public List<DiseaseResponse> getAll() {
+        List<Disease> diseases = this.repository.findAll();
+
+        return diseases.stream()
+                .map(d -> new DiseaseResponse(d))
+                .collect(Collectors.toList());
     }
 
 
