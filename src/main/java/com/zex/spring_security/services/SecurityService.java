2dc15f64f5657b3eaaeb88ce52e119c55e26a09f
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-25 09:11:33 -0300

    feat: 'implements SecurityService'

---
+++ src/main/java/com/zex/spring_security/services/SecurityService.java
@@ -0,0 +1,18 @@
+package com.zex.spring_security.services;
+
+import com.zex.spring_security.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.repository.findByLogin(username);
+    }
+}
