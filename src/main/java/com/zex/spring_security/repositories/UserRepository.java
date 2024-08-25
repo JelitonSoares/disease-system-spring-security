41623c9f316eeb4beec641fb4d3d1e507c2e9575
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-25 09:09:21 -0300

    feat: 'implements UserRepository and method findByLogin'

---
+++ src/main/java/com/zex/spring_security/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.zex.spring_security.repositories;
+
+import com.zex.spring_security.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByLogin(String login);
+}
