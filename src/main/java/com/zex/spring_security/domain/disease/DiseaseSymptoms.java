a53d1311a0fed087f58028d8ac527c14c7ead9be
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-25 08:42:16 -0300

    feat: 'add validations in DiseaseSymptoms'

--- src/main/java/com/zex/spring_security/domain/disease/DiseaseSymptoms.java
+++ src/main/java/com/zex/spring_security/domain/disease/DiseaseSymptoms.java
@@ -2,5 +2,5 @@ package com.zex.spring_security.domain.disease;
 
 import jakarta.validation.constraints.NotNull;
 
-public record DiseaseSymptoms(@NotNull String symptoms) {
+public record DiseaseSymptoms(@NotNullvString symptoms) {
 }
