7973571b1773cb71e04665a9ae649a8c80c88043
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-25 12:23:01 -0300

    feat: 'add filter order'

--- src/main/java/com/zex/spring_security/infra/security/SecurityConfigurations.java
+++ src/main/java/com/zex/spring_security/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.zex.spring_security.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -26,6 +31,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "auth/signup").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/disease").hasRole("ADMIN")
                         .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
