3355ef7f133054949bb41d1e06b908fbaa2dad82
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-08-25 10:25:32 -0300

    feat: 'implements custom constructor method '

--- src/main/java/com/zex/spring_security/domain/user/User.java
+++ src/main/java/com/zex/spring_security/domain/user/User.java
@@ -27,6 +27,12 @@ public class User implements UserDetails {
     private String password;
     private UserRole role;
 
+    public User(String login, String password, UserRole role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if (this.role == UserRole.ADMIN) {
