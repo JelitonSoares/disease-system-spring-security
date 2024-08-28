8dd2fd1e0da1c01fc99a6dfd0e77cb9f6438928b
JelitonSoares <jelitonn.soarres@gmail.com>
2024-08-28 20:31:40 -0300

    feat: 'change access modifiers of constructor method DiseaseUpdateDTO'

--- src/main/java/com/zex/spring_security/domain/disease/DiseaseUpdateDTO.java
+++ src/main/java/com/zex/spring_security/domain/disease/DiseaseUpdateDTO.java
@@ -2,7 +2,7 @@ package com.zex.spring_security.domain.disease;
 
 public record DiseaseUpdateDTO(String id, String cid, String name, String symptoms, String treatments, Integer risk) {
 
-    DiseaseUpdateDTO(Disease disease) {
+    public DiseaseUpdateDTO(Disease disease) {
         this(disease.getId(), disease.getCid(), disease.getName(), disease.getSymptoms(), disease.getTreatments(), disease.getRisk());
     }
 }
