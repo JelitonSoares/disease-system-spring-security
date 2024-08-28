553015771484142f6208a97478589e2db5189fa1
JelitonSoares <jelitonn.soarres@gmail.com>
2024-08-28 20:39:16 -0300

    feat: 'add validations on field id'

--- src/main/java/com/zex/spring_security/domain/disease/DiseaseUpdateDTO.java
+++ src/main/java/com/zex/spring_security/domain/disease/DiseaseUpdateDTO.java
@@ -1,6 +1,8 @@
 package com.zex.spring_security.domain.disease;
 
-public record DiseaseUpdateDTO(String id, String cid, String name, String symptoms, String treatments, Integer risk) {
+import jakarta.validation.constraints.NotBlank;
+
+public record DiseaseUpdateDTO(@NotBlank String id, String cid, String name, String symptoms, String treatments, Integer risk) {
 
     public DiseaseUpdateDTO(Disease disease) {
         this(disease.getId(), disease.getCid(), disease.getName(), disease.getSymptoms(), disease.getTreatments(), disease.getRisk());
