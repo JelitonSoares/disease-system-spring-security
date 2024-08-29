04d4c78a0d558323ab7120ccc86c34885314c966
JelitonSoares <jelitonn.soarres@gmail.com>
2024-08-28 21:06:00 -0300

    feat: 'implements new methods for query'

--- src/main/java/com/zex/spring_security/repositories/DiseaseRepository.java
+++ src/main/java/com/zex/spring_security/repositories/DiseaseRepository.java
@@ -4,8 +4,13 @@ import com.zex.spring_security.domain.disease.Disease;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DiseaseRepository extends JpaRepository<Disease, String> {
 
     List<Disease> findBySymptomsContainingIgnoreCase(String Symptoms);
+
+    List<Disease> findByCidContainingIgnoreCase(String cid);
+
+    List<Disease> findByNameContainingIgnoreCase(String name);
 }
